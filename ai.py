99e0057a321c82a3732eb42791de4bcffbe4a7ce
Benjamin Roziere <benjamin.roziere@rdalambic.com>
2017-06-21 18:54:40 +0200

    Change train signature

--- ai.py
+++ ai.py
@@ -14,6 +14,7 @@ class BernardAi:
         Représente une AI capable de jouer au jeu
         Implémente un réseau de neurones TensorFlow
     """
+
     def __init__(self, name):
         self.name = name
         self.x = None
@@ -48,12 +49,12 @@ class BernardAi:
             self.y = tf.nn.softmax(tf.matmul(self.x, self.W) + self.b)
             tf.summary.histogram('pre_activation', self.y)
 
-
-    def train(self, data_set, data_result, train_count):
+    def train(self, data_set, data_result, train_count, test_part_size, train_cycle, train_batch):
         """
             Entraine le réseau de neuronne
         """
         print("Training on {} data".format(train_count))
+
         # Expected result y_ and cross_entropy error reduction model
         self.y_ = tf.placeholder(tf.float32, [None, 6])
         cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=self.y))
@@ -64,11 +65,11 @@ class BernardAi:
         sess = tf.InteractiveSession()
         tf.global_variables_initializer().run()
 
-        data_x, test_x, data_y, test_y = get_test_set(10, data_set, data_result)
+        data_x, test_x, data_y, test_y = get_test_set(test_part_size, data_set, data_result)
         print("Training set of {} and test set of {} data".format(len(data_x), len(test_x)))
 
-        for i in range(8):
-            batch_x, batch_y = get_training_batch(4, data_x, data_y)
+        for i in range(train_cycle):
+            batch_x, batch_y = get_training_batch(train_batch, data_x, data_y)
             sess.run(train_step, feed_dict={self.x: batch_x, self.y_: batch_y})
 
         merged = tf.summary.merge_all()
@@ -78,8 +79,6 @@ class BernardAi:
         accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
 
         print(sess.run(accuracy, feed_dict={self.x: test_x, self.y_: test_y}))
-
-
 
     def log(self, graph):
         file_writer = tf.summary.FileWriter('{}.log'.format(self.name), graph=graph)
--- main.py
+++ main.py
@@ -14,17 +14,13 @@ def compile_config(argv, config):
 
 
 def create_training_set(path, test_set, test_result):
-    i = 0
 
     with open(path, 'rb') as file:
         while True:
             img = file.read(687126)
             if not img: break
-            i = i + 1
-            print(i)
             result = file.read(1)
             res_array = [0, 0, 0, 0, 0, 0]
-            print(result)
             res_array[int.from_bytes(result, 'big') - 1] = 1
             test_result.append(res_array)
             image = ImageOps.mirror((Image.frombytes('RGB', (638, 359), img)).convert('L').rotate(180))
@@ -39,37 +35,8 @@ def main():
 
     ai = BernardAi("Bernardo")
     ai.init(638, 359)
-    ai.train(test_set, test_result, len(test_result))
+    ai.train(test_set, test_result, len(test_result), 10, 10, 4)
     # ai.save()
-
-    """config = {
-        "mode": "base"
-    }
-
-    #compile_config(sys.argv, config)
-
-    print(config)
-    #Image input
-    x = tf.placeholder(tf.float32, [None, 100])
-
-    #Node and bias
-    W = weight_variable([100, 6])
-    b = bias_variable([6])
-
-    #Result
-    y = tf.nn.softmax(tf.matmul(x, W) + b)
-
-    #Expected result y_ and cross_entropy error reduction model
-    y_ = tf.placeholder(tf.float32, [None, 6])
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-
-    #Training
-    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-    #Session
-    sess = tf.InteractiveSession()
-
-    tf.global_variables_initializer().run()"""
 
 
 if __name__ == "__main__":
